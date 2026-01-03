a6f708e19fd027a3fe583a38c1b359ccd5404527
fahlaoui <fahlaouimohammed@gmail.com>
2026-01-03 16:15:08 +0100

    profile+recharge design

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import {ProfileComponent} from './layout/profile/profile.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RechargeComponent} from './layout/recharge/recharge.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    ProfileComponent,
+    RechargeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
