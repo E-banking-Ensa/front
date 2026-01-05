b3d422ed070be54db2807ff3f2c7cdd4eaa07871
haytam <haytams2210@gmail.com>
2026-01-05 22:35:02 +0100

    before merge

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackofficeLayoutComponent } from './layout/backoffice-layout/backoffice-layout.component';
-import {ProfileComponent} from './layout/profile/profile.component';
-import {RechargeComponent} from './layout/recharge/recharge.component';
-import {SidebarClientComponent} from './layout/sidebar-client/sidebar-client.component';
+import { ProfileComponent } from './layout/profile/profile.component';
+import { RechargeComponent } from './layout/recharge/recharge.component';
+import { SidebarClientComponent } from './layout/sidebar-client/sidebar-client.component';
 
 import { UserLogin } from './layout/user-login/user-login';
 import { UserRegistration } from './layout/user-registration/user-registration';
@@ -24,7 +24,7 @@ const routes: Routes = [
 
   },
   {
-    path : 'client',
+    path: 'client',
     loadComponent: () =>
       import('./layout/sidebar-client/sidebar-client.component')
         .then(c => c.SidebarClientComponent),
@@ -33,8 +33,8 @@ const routes: Routes = [
         .then(m => m.ClientRoutingModule)
   },
   { path: '', redirectTo: '/admin', pathMatch: 'full' },
-  {path: 'profile', component: ProfileComponent},
-  {path: 'recharge', component: RechargeComponent}
+  { path: 'profile', component: ProfileComponent },
+  { path: 'recharge', component: RechargeComponent }
 ];
 
 
